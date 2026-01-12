ff4d1606ba3a499a3cdbb8d05cee4e78f7581082
Boris THERON <boris.theron@gmail.com>
2026-01-12 17:08:47 +0100

    add ActivityRepo

--- src/ports/driven/repoPort.ts
+++ src/ports/driven/repoPort.ts
@@ -1,7 +1,15 @@
+import { Activity } from '../../domain/activity';
 import { User } from '../../domain/user';
 
 export interface UserRepositoryPort {
   findAll(): Promise<User[]>;
   findById(id: string): Promise<User | null>;
   save(user: Omit<User, 'id'>): Promise<User>;
+}
+
+
+export interface ActivityRepositoryPort {
+  findAll(): Promise<Activity[]>;
+  findById(id: string): Promise<Activity | null>;
+  save(activity: Omit<Activity, 'id'>): Promise<Activity>;
 }
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import * as YAML from 'yaml';
 import swaggerUi from 'swagger-ui-express';
-import addressController from './adapters/driving/addressController';
+import userController from './adapters/driving/userController';
 import path from 'path';
 import * as fs from "node:fs";
 
@@ -13,7 +13,7 @@ const swaggerDocument = YAML.parse(file)
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use('/addresses', addressController);
+app.use('/users', userController);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
