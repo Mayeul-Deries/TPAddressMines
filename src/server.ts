f269af1eed6759832e896b32e174f15a7bd74d35
Morgan Faget <morgan.faget@getyooz.com>
2026-01-12 17:02:11 +0100

    Changing the registering of controller

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,24 @@
 import express from 'express';
-import * as YAML from 'yaml';
-import swaggerUi from 'swagger-ui-express';
-import addressController from './adapters/driving/addressController';
 import path from 'path';
 import * as fs from "node:fs";
+import * as YAML from 'yaml';
+import swaggerUi from 'swagger-ui-express';
+
+import { AddressController } from './adapters/driving/addressController';
+import { InMemoryAddressRepo } from "./adapters/driven/inMemoryAddressRepo";
 
 const app = express();
 app.use(express.json());
 
+const addressRepo = new InMemoryAddressRepo();
+
 const file  = fs.readFileSync('./openapi.yaml', 'utf8')
 const swaggerDocument = YAML.parse(file)
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use('/addresses', addressController);
+const addressController = new AddressController(addressRepo);
+addressController.registerRoutes(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
