c65c50ff1422a954757e24252834276788ec47d1
Morgan Faget <morgan.faget@getyooz.com>
2026-01-12 17:21:00 +0100

    Testing inMemoryAddressRepo

--- src/adapters/driven/inMemoryAddressRepo.spec.ts
+++ src/adapters/driven/inMemoryAddressRepo.spec.ts
@@ -1,23 +1,32 @@
-// import { Address } from '../../domain/address';
-// import { AddressRepositoryPort } from '../../ports/driven/repoPort';
-// import { v4 as uuidv4 } from 'uuid';
-// import { InMemoryAddressRepo } from './inMemoryAddressRepo';
-//
-// const store: Address[] = [];
-//
-// export class InMemoryAddressRepo implements AddressRepositoryPort {
-//   async findAll(): Promise<Address[]> {
-//     return store.slice();
-//   }
-//
-//   async findById(id: string): Promise<Address | null> {
-//     const found = store.find((s) => s.id === id);
-//     return found ?? null;
-//   }
-//
-//   async save(address: Omit<Address, 'id'>): Promise<Address> {
-//     const newAddress: Address = { id: uuidv4(), ...address };
-//     store.push(newAddress);
-//     return newAddress;
-//   }
-// }
+import { Address } from '../../domain/address';
+import { AddressRepositoryPort } from '../../ports/driven/repoPort';
+import { v4 as uuidv4 } from 'uuid';
+import { InMemoryAddressRepo } from './inMemoryAddressRepo';
+
+describe('inMemoryAddressRepo', () => {
+  let repo: InMemoryAddressRepo;
+  let addresses: Address[] = [];
+
+  beforeEach(async () => {
+    addresses = [];
+    repo = new InMemoryAddressRepo(addresses);
+  })
+
+  it('should save an address', async () => {
+    const addressData = new Address('123 Main St','Anytown',12345);
+    const savedAddress = await repo.save(addressData);
+
+    expect(savedAddress).toHaveProperty('id');
+    expect(savedAddress.street).toBe(addressData.street);
+    expect(addresses.length).toBe(1);
+  });
+
+  it('should get all addresses by duplicating variable', async () => {
+    addresses = [new Address('123 Main St','Anytown',12345)];
+    repo = new InMemoryAddressRepo(addresses);
+    const allAddresses = await repo.findAll();
+
+    expect(allAddresses).toEqual(addresses);
+    expect(allAddresses).not.toBe(addresses); // Ensure it's a copy
+  });
+})
